3b03f9b5fb5dd743731b76e1232826514c66f72f
Maq301 <franciscoklf@netscape.net>
2019-05-21 20:03:22 -0300

    corregida la ruta de la base de datos. ahora no da error de codigo.

--- db.py
+++ db.py
@@ -1,5 +1,5 @@
 # Aca va el codigo de la base de datos.Aca
 import sqlite3
 def base_datos():
-    base = sqlite3.connect('C:\Users\Maq301\Documents\generala\generala\generala.db')
+    base = sqlite3.connect('C:/Users/Maq301/PycharmProjects/generala')
     c = base.cursor()
