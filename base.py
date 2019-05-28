e9f7f46c321d0a579113d71b411e8b4c18720168
Maq301 <franciscoklf@netscape.net>
2019-05-28 19:36:41 -0300

    Lo anterior estaba comentado para el orto. ahora esta bien escrito.

--- base.py
+++ base.py
@@ -3,11 +3,13 @@ import os
 
 '''
 
-base = sqlite3.connect('C:/Users/Maq301/PycharmProjects/generala/generala.db')
-cursor = base.cursor()
+
 '''
 # Aclaracion para provar el push al testing.
 
 dirname = os.path.dirname(__file__)
 filename = os.path.join(dirname, 'generala.db')
+base = sqlite3.connect(filename)
+cursor = base.cursor()
+
 print(filename)
