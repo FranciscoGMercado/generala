02a11b4eaa8d54caa712a3d2dc3262a1ebbd3171
Maq301 <franciscoklf@netscape.net>
2019-05-28 19:31:25 -0300

    base.py bien escrito. y con el ejemplo para hacer el path sea el directorio en el que este ande.

---
+++ base.py
@@ -0,0 +1,11 @@
+import sqlite3
+'''
+import os
+dirname = os.path.dirname(__file__)
+filename = os.path.join(dirname, 'generala.db')
+
+'''
+# Aclaracion para provar el push al testing.
+
+base = sqlite3.connect('C:/Users/Maq301/PycharmProjects/generala/generala.db')
+cursor = base.cursor()
