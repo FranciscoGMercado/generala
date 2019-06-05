72f09c5fea9093e64af438aa198a70ae1cd9ee05
Maq301 <franciscoklf@netscape.net>
2019-06-05 19:46:30 -0300

    actualizacion de dos archivos .py

--- db.py
+++ db.py
@@ -3,3 +3,19 @@ import sqlite3
 def base_datos():
     base = sqlite3.connect('C:/Users/Maq301/PycharmProjects/generala')
     c = base.cursor()
+
+def jugadores():
+    j = []  # J por jugadores.
+    n = int(input('Ingrese el numero de jugadores para esta partida: '))
+    for i in range(0, n):
+        jugador = str(input('ingrese el nombre del jugador: '))
+        j.append(jugador)
+    for s in j:
+        print(s)
+    based = sqlite3.connect('generala.db')
+    c = based.cursor()
+    '''La variable va entre corchetes como una lista, si no toma solo una letra,
+    sobran las demas y tira un error'''
+    c.execute('insert into jugadores(nombre_jugadores) values(?)', [j])
+    based.commit()
+    based.close()
