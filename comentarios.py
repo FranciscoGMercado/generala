8f259d71183b49e352c1dcebf08a82d66bccd5f4
Gerardo <franciscoklf@netscape.net>
2019-06-19 18:03:17 -0300

    Delete comentarios.py
    Estoy re-haciendo el programa desde el testing.

--- comentarios.py
+++
@@ -1,3 +0,0 @@
-# Con dobre comillas puedo hacer comentarios en mas de una linea.
-## github primero hago add. luego commit que es hacer un comentario. y en tercer # y
-# ultimo lugar un puxh.- add - commit - push.
